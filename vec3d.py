8c145d49d2291091d2c56eb6247d85c390117136
suniique <32534476+suniique@users.noreply.github.com>
2018-06-22 16:01:49 -0700

    [+] add the mid point algorithm

---
+++ vec3d.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def midPoint(X1, W1, X2, W2):
+    ''' calulate the mid-point of the Common Perpendicular of lines on different planes'''
+    W1 = np.mat(W1).T / np.linalg.norm(W1)
+    W2 = np.mat(W2).T / np.linalg.norm(W2)
+    X1 = np.mat(X1).T
+    X2 = np.mat(X2).T
+
+    T1 = np.mat(np.diag([1, 1, 1])) - W1 * W1.T / (W1.T * W1)
+    T2 = np.mat(np.diag([1, 1, 1])) - W2 * W2.T / (W2.T * W2)
+
+    P = (T1 + T2).I * (T1 * X1 + T2 * X2)
+    return P
+
+
+def main():
+    p = midPoint([0, 0, 0], [1, 0, 0], [1, 1, 1], [0, -1, 0])
+    print(p)
+
+
+if __name__ == '__main__':
+    main()
